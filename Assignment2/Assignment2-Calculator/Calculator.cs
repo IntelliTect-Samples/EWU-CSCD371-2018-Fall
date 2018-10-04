c29d3f1cddd403da9b4c8a55d30884d1fdb2b31f
Casey White <caseywhite534@gmail.com>
2018-10-03 22:46:35 -0700

    Added Unit Tests for subtraction

--- Assignment2/Assignment2-Calculator/Calculator.cs
+++ Assignment2/Assignment2-Calculator/Calculator.cs
@@ -4,21 +4,20 @@ using System.Text.RegularExpressions;
 
 namespace Assignment2_Calculator
 {
-    public class Calculator
+    public static class Calculator
     {  
         public static void Main(string[] args)
         {   
             var userInput = args.Length == 0 ? TakeUserInput() : args[0];
-            int max = int.MaxValue;
             while(!IsValidExpression(userInput))
                 userInput = TakeUserInput();
 
             try
             {
-                (int operandOne, int operandTwo, char operatorOne)
-                    expressionTuple = ParseExpressionIntoTuple(userInput);
+                (long operandOne, long operandTwo, char operatorOne)
+                    expressionTuple = ParseIntoTuple(userInput);
 
-                double solution = EvaluateExpression(expressionTuple);
+                var solution = EvaluateExpression(expressionTuple);
                 Console.WriteLine(
                     $"{expressionTuple.operandOne}{expressionTuple.operatorOne}{expressionTuple.operandTwo}={solution}");
             }
@@ -31,8 +30,7 @@ namespace Assignment2_Calculator
         private static string TakeUserInput()
         {
             Console.Write("Please enter an expression ie. (3+1): ");
-            string input = Console.ReadLine();
-            return Regex.Replace(input, " ", "");
+            return Regex.Replace(Console.ReadLine(), " ", "");
         }
 
 
@@ -45,58 +43,34 @@ namespace Assignment2_Calculator
             return isValid;
         }
 
-        private static (int, int, char) ParseExpressionIntoTuple(string expressionStr)
+        private static (long, long, char) ParseIntoTuple(string expressionStr)
         {
-            (int operandOne, int operandTwo, char operatorOne) expressionTuple; 
+            (long operandOne, long operandTwo, char operatorOne) expressionTuple; 
             var regex = new Regex(@"(?<=\d)[-+*/]{1}");        //gets operator not minus sign
 
-           var match = regex.Match(expressionStr);
+            var match = regex.Match(expressionStr);
             char.TryParse(match.Value, out expressionTuple.operatorOne);
             string[] splitExpression = expressionStr.Split(expressionTuple.operatorOne);
-            int.TryParse(splitExpression[0], out expressionTuple.operandOne);
-            int.TryParse(splitExpression[1], out expressionTuple.operandTwo);
+            long.TryParse(splitExpression[0], out expressionTuple.operandOne);
+            long.TryParse(splitExpression[1], out expressionTuple.operandTwo);
             return expressionTuple;
         }
 
-        private static double EvaluateExpression( (int operandOne, int operandTwo, char operatorOne) expressionTuple)
+        private static double EvaluateExpression( (long operandOne, long operandTwo, char operatorOne) expressionTuple)
         {
             switch (expressionTuple.operatorOne)
             {
                 case '+':
                     return expressionTuple.operandOne + expressionTuple.operandTwo;
                 case '-':
-                    return expressionTuple.operandOne + expressionTuple.operandTwo;
+                    return expressionTuple.operandOne - expressionTuple.operandTwo;
                 case '*':
-                    return expressionTuple.operandOne + expressionTuple.operandTwo;
+                    return expressionTuple.operandOne * expressionTuple.operandTwo;
                 case '/':
-                    return expressionTuple.operandOne + expressionTuple.operandTwo;
+                    return expressionTuple.operandOne / (double)expressionTuple.operandTwo;
                 default:
                     return 0;
             }
-        }
-        
-        
-
-        private static int Add(int intA, int intB)
-        {
-            return intA + intB;
-        }
-        
-        private static int Subtract(int intA, int intB)
-        {
-            return intA - intB;
-        }
-
-        private static double Multiply(int intA, int intB)
-        {
-            return intA * intB;
-        }
-
-        private static double Divide(int intA, int intB)
-        {
-            if(intB == 0)
-                throw new DivideByZeroException();
-            return intA / (double)intB;
         }
     }
 }
--- Assignment2/CalculatorTest/CalculatorTester.cs
+++ Assignment2/CalculatorTest/CalculatorTester.cs
@@ -7,26 +7,30 @@ namespace CalculatorTest
     [TestClass]
     public class InputTests
     {
+        /**
+         * Note that for tester simplicity i did not include spaces in the
+         * input with the exception of this method
+         */
         [DataRow("5 + 5", "10")]
         [DataRow("5 +9", "14")]
         [DataRow("1+3", "4")]
         [DataTestMethod]
         public void UserInputPromptValid(string inputStr, string answer)
         {
-            string inputStrNoSpace = Regex.Replace(inputStr, " ", "");
+            var inputStrNoSpace = Regex.Replace(inputStr, " ", "");
             var ExpectedOutput = $@">>Please enter an expression ie. (3+1): <<{inputStr}
             >>{inputStrNoSpace}={answer}";
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(ExpectedOutput, Assignment2_Calculator.Calculator.Main);
         }
 
         [DataRow("99")]
+        [DataRow("--9/80")]
         [DataRow("")]
         [DataRow(".5*999")]
         [DataRow("1plus3")]
         [DataTestMethod]
         public void UserInputPromptInvalid(string inputStr)
         {
-            Assert.IsFalse(false);
             var ExpectedOutput = $@">>Please enter an expression ie. (3+1): <<{inputStr}
 >>Invalid Expression: {inputStr}
 >>Please enter an expression ie. (3+1): <<1+1
@@ -48,9 +52,8 @@ namespace CalculatorTest
         [DataTestMethod]
         public void AddPositive(string inputStr, string answer)
         {
-            string inputStrNoSpace = Regex.Replace(inputStr, " ", "");
             var ExpectedOutput = $@">>Please enter an expression ie. (3+1): <<{inputStr}
-            >>{inputStrNoSpace}={answer}";
+            >>{inputStr}={answer}";
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(ExpectedOutput, Assignment2_Calculator.Calculator.Main);
         }
         
@@ -60,9 +63,8 @@ namespace CalculatorTest
         [DataTestMethod]
         public void AddNegPlusPos(string inputStr, string answer)
         {
-            string inputStrNoSpace = Regex.Replace(inputStr, " ", "");
             var ExpectedOutput = $@">>Please enter an expression ie. (3+1): <<{inputStr}
-            >>{inputStrNoSpace}={answer}";
+            >>{inputStr}={answer}";
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(ExpectedOutput, Assignment2_Calculator.Calculator.Main);
         }
         
@@ -72,21 +74,99 @@ namespace CalculatorTest
         [DataTestMethod]
         public void AddPosPlusNeg(string inputStr, string answer)
         {
-            string inputStrNoSpace = Regex.Replace(inputStr, " ", "");
             var ExpectedOutput = $@">>Please enter an expression ie. (3+1): <<{inputStr}
-            >>{inputStrNoSpace}={answer}";
+            >>{inputStr}={answer}";
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(ExpectedOutput, Assignment2_Calculator.Calculator.Main);
         }
         
-        [DataRow("50+-5", "45")]
-        [DataRow("100+-10000", "-9900")]
-        [DataRow("9999+-1", "9998")]
+        [DataRow("2147483647+2", "2147483649")]
+        [DataRow("2+2147483647", "2147483649")]
+        [DataRow("2147483647+-100", "2147483547")]
+        [DataRow("2147483647+2147483647", "4294967294")]
         [DataTestMethod]
         public void AddMaxValue(string inputStr, string answer)
         {
-            string inputStrNoSpace = Regex.Replace(inputStr, " ", "");
             var ExpectedOutput = $@">>Please enter an expression ie. (3+1): <<{inputStr}
-            >>{inputStrNoSpace}={answer}";
+            >>{inputStr}={answer}";
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(ExpectedOutput, Assignment2_Calculator.Calculator.Main);
+        }
+        
+        [DataRow("-2147483647+2", "-2147483645")]
+        [DataRow("2+-2147483647", "-2147483645")]
+        [DataRow("-2147483647+-100", "-2147483747")]
+        [DataRow("-2147483647+-2147483647", "-4294967294")]
+        [DataTestMethod]
+        public void AddMinValue(string inputStr, string answer)
+        {
+            var ExpectedOutput = $@">>Please enter an expression ie. (3+1): <<{inputStr}
+            >>{inputStr}={answer}";
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(ExpectedOutput, Assignment2_Calculator.Calculator.Main);
+        }
+    }
+    
+    
+    
+    
+    
+    
+    [TestClass]
+    public class SubtractionIntegerTests
+   
+    {
+        [DataRow("50-5", "45")]
+        [DataRow("100-10000", "-9900")]
+        [DataRow("9999-1", "9998")]
+        [DataTestMethod]
+        public void SubtractPositive(string inputStr, string answer)
+        {
+            var ExpectedOutput = $@">>Please enter an expression ie. (3+1): <<{inputStr}
+            >>{inputStr}={answer}";
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(ExpectedOutput, Assignment2_Calculator.Calculator.Main);
+        }
+        
+        [DataRow("-50-5", "-55")]
+        [DataRow("-100-10000", "-10100")]
+        [DataRow("-9999-1", "-10000")]
+        [DataTestMethod]
+        public void SubtractNegMinusPos(string inputStr, string answer)
+        {
+            var ExpectedOutput = $@">>Please enter an expression ie. (3+1): <<{inputStr}
+            >>{inputStr}={answer}";
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(ExpectedOutput, Assignment2_Calculator.Calculator.Main);
+        }
+        
+        [DataRow("50--5", "55")]
+        [DataRow("10--10000", "10100")]
+        [DataRow("9999--1", "1000")]
+        [DataTestMethod]
+        public void SubtractPosMinusNeg(string inputStr, string answer)
+        {
+            var ExpectedOutput = $@">>Please enter an expression ie. (3+1): <<{inputStr}
+            >>{inputStr}={answer}";
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(ExpectedOutput, Assignment2_Calculator.Calculator.Main);
+        }
+        
+        [DataRow("2147483647-2", "2147483645")]
+        [DataRow("-2-2147483647", "-2147483649")]
+        [DataRow("2147483647-100", "2147483547")]
+        [DataRow("-2147483647-2147483647", "-4294967294")]
+        [DataTestMethod]
+        public void SubtractMaxValue(string inputStr, string answer)
+        {
+            var ExpectedOutput = $@">>Please enter an expression ie. (3+1): <<{inputStr}
+            >>{inputStr}={answer}";
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(ExpectedOutput, Assignment2_Calculator.Calculator.Main);
+        }
+        
+        [DataRow("-2147483647-2", "2147483649")]
+        [DataRow("2--2147483647", "2147483645")]
+        [DataRow("-2147483647-100", "-2147483747")]
+        [DataRow("-2147483647--2147483647", "0")]
+        [DataTestMethod]
+        public void SubtractMinValue(string inputStr, string answer)
+        {
+            var ExpectedOutput = $@">>Please enter an expression ie. (3+1): <<{inputStr}
+            >>{inputStr}={answer}";
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(ExpectedOutput, Assignment2_Calculator.Calculator.Main);
         }
     }
