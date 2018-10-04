7f1414f1c3441609407644b15344dfdee3f66c81
Cameron Osborn <cameron0401@gmail.com>
2018-10-03 19:05:23 -0700

    Collapse static helper class and move methods to "Program".

--- Assignment2/ConsoleMathSolver/Program.cs
+++ Assignment2/ConsoleMathSolver/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ConsoleMathSolver
 {
@@ -12,7 +14,7 @@ namespace ConsoleMathSolver
             {
                 Console.Write("Please enter expression in form <integer><operator><integer>: ");
                 string userInput = Console.ReadLine();
-                double result = ConsoleMathSolverHelper.CalculateValue(userInput);
+                double result = CalculateValue(userInput);
                 Console.WriteLine($"Value is: {result}");
             }
             catch (Exception e)
@@ -20,6 +22,138 @@ namespace ConsoleMathSolver
                 Console.WriteLine(e);
                 throw;
             }
+        }
+        
+        public static double CalculateValue(string userInput)
+        {
+            if (string.IsNullOrEmpty(userInput))
+            {
+                throw new InvalidDataException("Input cannot be empty!");
+            }
+            
+            // remove spaces from input string
+            string userInputNoSpaces = Regex.Replace(userInput, "\\ +", "");
+
+            if (userInputNoSpaces.Length == 0)
+            {
+                throw new InvalidDataException("Input cannot be empty!");
+            }
+           
+            string operatorUsed = OperatorUsed(userInputNoSpaces);
+
+            List<int> parsedNumbers = ParseOperators(userInputNoSpaces, operatorUsed);
+
+            if (parsedNumbers.Count != 2)
+            {
+                return 0; 
+            }
+            double result = (double) parsedNumbers[0];
+
+            switch (operatorUsed)
+            {
+                    case "+":
+                        for (int i = 1; i < parsedNumbers.Count; i++)
+                        {
+                            result += parsedNumbers[i];
+                        }
+
+                        return result;
+                    case "-":
+                        for (int i = 1; i < parsedNumbers.Count; i++)
+                        {
+                            result -= parsedNumbers[i];
+                        }
+
+                        return result;
+                    case "*":
+                        for (int i = 1; i < parsedNumbers.Count; i++)
+                        {
+                            result *= parsedNumbers[i];
+                        }
+
+                        return result;
+                    case "/":
+                        for (int i = 1; i < parsedNumbers.Count; i++)
+                        {
+                            // check for divide by 0
+                            if (parsedNumbers[i] == 0)
+                            {
+                                throw new DivideByZeroException("Cannot divide by 0!");
+                            }
+
+                        result /= parsedNumbers[i];
+                        }
+
+                        return result;
+            }
+            
+            return result;
+        }
+        
+        // throws NullReferenceException if operatorInput or operatorUsed are null
+        // throws InvalidDataException 
+        private static List<int> ParseOperators(string operatorInput, string operatorUsed)
+        {
+            List<int> toReturn = new List<int>();
+
+            List<string> tempArr = new List<string>(operatorInput.Split(operatorUsed));
+
+            // no operators present
+            if (tempArr.Count == 0)
+            {
+                throw new InvalidDataException("Invalid input sequence! Input must match <integer><operator><integer>");
+            }
+
+            int validNumCount = 0;
+
+            foreach (var temp in tempArr)
+            {
+                if (int.TryParse(temp, out int parsedVal))
+                {
+                    toReturn.Add(parsedVal);
+                }
+                if (!temp.Equals(""))
+                {
+                    validNumCount++;
+                }
+            }
+
+            // too many operators
+            return (validNumCount == tempArr.Count) ? toReturn : 
+                throw new InvalidDataException("String not properly operator delimited.");
+        }
+
+        // Finds first non-digit in string, and determines if it is a valid operator
+        // Throws InvalidDataException if no valid operator found
+        private static string OperatorUsed(string operatorInput)
+        {
+            for (int i = 0; i < operatorInput.Length; i++)
+            {
+                if (i == 0 && operatorInput[i] == '-')
+                {
+                    continue;
+                }
+                if (!char.IsDigit(operatorInput[i]))
+                {
+                    if (operatorInput[i] == '-' && (i+1) < operatorInput.Length)
+                    {
+                        // -1 - -1
+                        // negative following subtraction symbol
+                        if (operatorInput[i + 1] == '-')
+                        {
+                            return "--";
+                        }
+                    }
+                    else if (new List<char> {'+', '-', '/', '*'}.Contains(operatorInput[i]))
+                    {
+                        return operatorInput[i] + "";
+                    }
+
+                    throw new InvalidDataException($"\"{i}\" is not a valid operator");
+                }
+            }
+
+            throw new InvalidDataException("No valid operator found");
         }
     }
 }
