7b31ef69f695af1b0812834185b01d744f5b54d2
Cameron Osborn <cameron0401@gmail.com>
2018-10-03 21:51:14 -0700

    Implement test of String insert method.

--- Assignment2/ConsoleMathSolver.Tests/StringTests.cs
+++ Assignment2/ConsoleMathSolver.Tests/StringTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ConsoleMathSolver.Tests
@@ -5,9 +6,17 @@ namespace ConsoleMathSolver.Tests
     [TestClass]
     public class StringTests
     {
+        // remove
+        // trim
+        // ends with
         [TestMethod]
-        public void TestMethod()
+        [DataRow("someinsidething", "something", "inside", 4)]
+        [DataRow("therehing", "thing", "here", 1)]
+        [DataRow("setfirst", "first", "set", 0)]
+        [DataRow("lastset", "last", "set", 4)]
+        public void TestStringInsert(string expected, string toInsertInto, string insertThis, int atIndex)
         {
+            Assert.AreEqual(expected, toInsertInto.Insert(atIndex, insertThis));
         }
     }
 }
