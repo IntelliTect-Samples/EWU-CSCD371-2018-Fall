55436ef0507a95e47ba943f49c3a8fd031e310f3
Casey White <caseywhite534@gmail.com>
2018-10-10 19:43:38 -0700

    Tests the CalculateDamageDealt method

---
+++ Assignment3/RockPaperScissorsTest/TestCalculateDamageDealt.cs
@@ -0,0 +1,73 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RockPaperScissorsTest
+{
+
+    /*
+     * CalculateDamageDealt method returns:
+     * 20 for rock win
+     * 10 for paper win
+     * 15 for scissors win
+     * 0 for draw
+     * these values are negative if cpu wins, positive if user wins
+     */
+    [TestClass]
+    public class TestCalculateDamageDealt
+    {
+        [TestMethod]
+        public void TestUserBeatsCpuWithRock()
+        {
+            Assert.AreEqual(20, RockPaperScissors.RockPaperScissors.CalculateDamageDealt(("rock", "scissors")));
+        }
+        
+        [TestMethod]
+        public void TestUserBeatsCpuWithPaper()
+        {
+            Assert.AreEqual(10, RockPaperScissors.RockPaperScissors.CalculateDamageDealt(("paper", "rock")));
+        }
+        
+        [TestMethod]
+        public void TestUserBeatsCpuWithScissors()
+        {
+            Assert.AreEqual(15, RockPaperScissors.RockPaperScissors.CalculateDamageDealt(("scissors", "paper")));
+        }
+        
+        
+        [TestMethod]
+        public void TestCpuBeatsUserWithRock()
+        {
+            Assert.AreEqual(-20, RockPaperScissors.RockPaperScissors.CalculateDamageDealt(("scissors", "rock")));
+        }
+        
+        [TestMethod]
+        public void TestCpuBeatsUserWithPaper()
+        {
+            Assert.AreEqual(-10, RockPaperScissors.RockPaperScissors.CalculateDamageDealt(("rock", "paper")));
+        }
+        
+        [TestMethod]
+        public void TestCpuBeatsUserWithScissors()
+        {
+            Assert.AreEqual(-15, RockPaperScissors.RockPaperScissors.CalculateDamageDealt(("paper", "scissors")));
+        }
+        
+        [TestMethod]
+        public void TestDrawRock()
+        {
+            Assert.AreEqual(0, RockPaperScissors.RockPaperScissors.CalculateDamageDealt(("rock", "rock")));
+        }
+        
+        [TestMethod]
+        public void TestDrawPaper()
+        {
+            Assert.AreEqual(0, RockPaperScissors.RockPaperScissors.CalculateDamageDealt(("paper", "paper")));
+        }
+        
+        [TestMethod]
+        public void TestDrawScissors()
+        {
+            Assert.AreEqual(0, RockPaperScissors.RockPaperScissors.CalculateDamageDealt(("scissors", "scissors")));
+        }
+    }
+
+}
