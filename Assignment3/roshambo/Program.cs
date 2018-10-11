e1aaea4ce42f24d2175c948e2bc75e4750cf0669
Cameron Osborn <cameron0401@gmail.com>
2018-10-10 18:40:58 -0700

    Setup file structure

---
+++ Assignment3/roshambo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace roshambo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
