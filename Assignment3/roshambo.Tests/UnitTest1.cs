170fdc924932dd4b0aac891d65bfec2838b3646e
Cameron Osborn <cameron0401@gmail.com>
2018-10-10 20:23:25 -0700

    Create additional test to ensure proper point values are returned.

--- Assignment3/roshambo.Tests/UnitTest1.cs
+++ Assignment3/roshambo.Tests/UnitTest1.cs
@@ -14,10 +14,21 @@ namespace roshambo.Tests
         [DataRow("paper", "scissors", "PlayerTwo")]
         [DataRow("scissors", "paper", "PlayerOne")]
         [DataRow("scissors", "rock", "PlayerTwo")]
-        public void TestCalculateRoundLoser(string playerOneMove, string playerTwoMove, string expectedWinner)
+        public void TestCalculateRoundLoserString(string playerOneMove, string playerTwoMove, string expectedWinner)
         {
             (string winner, int healthDeduction) returnVal = Program.CalculateRoundLoser(playerOneMove, playerTwoMove);
             Assert.AreEqual(expectedWinner, returnVal.winner);
+        }
+
+        [DataTestMethod]
+        [DataRow("paper", "paper", 0)]
+        [DataRow("paper", "rock", 10)]
+        [DataRow("rock", "scissors", 20)]
+        [DataRow("scissors", "paper", 15)]
+        public void TestCalculateRoundLoserPoints(string playerOneMove, string playerTwoMove, int expectedPoints)
+        {
+            (string winner, int healthDeduction) returnVal = Program.CalculateRoundLoser(playerOneMove, playerTwoMove);
+            Assert.AreEqual(expectedPoints, returnVal.healthDeduction);
         }
     }
 }
