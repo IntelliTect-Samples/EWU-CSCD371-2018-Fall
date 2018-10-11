c418feeb6de5c1967372e558df96bf552cb678d6
Cameron Osborn <cameron0401@gmail.com>
2018-10-10 20:14:49 -0700

    Create method to determine round loser, and accompanying tests.

--- Assignment3/roshambo.Tests/UnitTest1.cs
+++ Assignment3/roshambo.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace roshambo.Tests
@@ -5,9 +6,18 @@ namespace roshambo.Tests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        [DataRow("rock", "rock", "tie")]
+        [DataRow("rock", "paper", "PlayerTwo")]
+        [DataRow("rock", "scissors", "PlayerOne")]
+        [DataRow("paper", "rock", "PlayerOne")]
+        [DataRow("paper", "scissors", "PlayerTwo")]
+        [DataRow("scissors", "paper", "PlayerOne")]
+        [DataRow("scissors", "rock", "PlayerTwo")]
+        public void TestCalculateRoundLoser(string playerOneMove, string playerTwoMove, string expectedWinner)
         {
+            (string winner, int healthDeduction) returnVal = Program.CalculateRoundLoser(playerOneMove, playerTwoMove);
+            Assert.AreEqual(expectedWinner, returnVal.winner);
         }
     }
 }
--- Assignment3/roshambo/Program.cs
+++ Assignment3/roshambo/Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.IO;
 
 namespace roshambo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// Given playerOneMove and playerTwoMove return the loser of the round and the number of health points
+        /// to deduct. Throws InvalidDataException is data does not evaluate to a valid move
+        /// </summary>
+        /// <param name="playerOneMove">Move of the first player. Valid values: rock, paper, scissors</param>
+        /// <param name="playerTwoMove">Move of the second player. Valid values: rock, paper, scissors</param>
+        /// <returns>"PlayerOne" if playerOne is the winner. "PlayerTwo" is playerTwo is the winner
+        /// "tie" if there is no winner.</returns>
+        public static (string, int) CalculateRoundLoser(string playerOneMove, string playerTwoMove)
+        {
+            string playerOneMoveLower = playerOneMove.ToLower();
+            string playerTwoMoveLower = playerTwoMove.ToLower();
+
+            bool swap = false; // if the moves for playerOne and playerTwo have been reversed
+            
+            if (!playerOneMoveLower.Equals(playerTwoMoveLower))
+            {
+                while (true)
+                {
+                    switch (playerOneMoveLower)
+                    {
+                        case "rock": // 20 points
+                            if (playerTwoMoveLower.Equals("scissors"))
+                            {
+                                int pointsVal = 20;
+                                return (swap) ? ("PlayerTwo", pointsVal) : ("PlayerOne", pointsVal);
+                            }
+                            break;
+                        case "paper": // 10 points
+                            if (playerTwoMoveLower.Equals("rock"))
+                            {
+                                int pointsVal = 10;
+                                return (swap) ? ("PlayerTwo", pointsVal) : ("PlayerOne", pointsVal);
+                            }
+                            break;
+                        case "scissors": // 15 points
+                            if (playerTwoMoveLower.Equals("paper"))
+                            {
+                                int pointsVal = 15;
+                                return (swap) ? ("PlayerTwo", pointsVal) : ("PlayerOne", pointsVal);
+                            }
+                            break;
+                        default:
+                            throw new InvalidDataException($"{playerOneMoveLower} is not a valid move");
+                    }
+                    
+                    string temp = playerTwoMoveLower;
+                    playerTwoMoveLower = playerOneMoveLower;
+                    playerOneMoveLower = temp;
+                    swap = true;
+                }
+            }
+            else
+            {
+                return ("tie", 0);
+            }
         }
     }
 }
