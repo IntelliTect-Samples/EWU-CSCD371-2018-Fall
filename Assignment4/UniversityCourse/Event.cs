b1ee49d5479ad913ee7edeec65683e9fc2e879fc
Casey White <caseywhite534@gmail.com>
2018-10-18 16:13:31 -0700

    Initial commit

---
+++ Assignment4/UniversityCourse/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityCourse
+{
+    public class Event
+    {
+        private string _TimeStart;
+        public string TimeStart
+        {
+            get { return _TimeStart; }
+            set { _TimeStart = value; }
+        }
+
+        public string TimeEnd{ get; set; }
+
+
+        public string GetSummaryInformation()
+        {
+            return null;
+        }
+    }
+}
