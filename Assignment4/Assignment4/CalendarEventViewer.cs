a713bd63c1833a4cc571a3bca33a02169463d3d7
Cameron Osborn <cameron0401@gmail.com>
2018-10-18 01:37:48 -0700

    Implement static class CalendarEventViewer Display().

--- Assignment4/Assignment4/CalendarEventViewer.cs
+++ Assignment4/Assignment4/CalendarEventViewer.cs
@@ -1,7 +1,26 @@
+using System;
+using System.IO;
+
 namespace Assignment4
 {
-    public class CalendarEventViewer
+    public static class CalendarEventViewer
     {
-        
+        public static string Display(Object toDisplay)
+        {
+            if (! (toDisplay is Event))
+            {
+                throw new InvalidDataException("Object must be a type of Event!");
+            }
+
+            switch (toDisplay)
+            {
+                 case UniversityCourse toUse:
+                     return toUse.GetSummaryInformation();
+                 case Event toUse:
+                     return toUse.GetSummaryInformation();
+                 default:
+                     return toDisplay.ToString();
+            }
+        }
     }
 }
