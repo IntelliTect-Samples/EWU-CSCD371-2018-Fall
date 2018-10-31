be017b76afd7232c2f88f1f9a8d47c6faba0f9e1
Cameron Osborn <cameron0401@gmail.com>
2018-10-30 18:12:06 -0700

    Implement testing on concrete implementation RealConsole.

--- Assignment5/Assignment5.Tests/TestRealConsole.cs
+++ Assignment5/Assignment5.Tests/TestRealConsole.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Assignment5.Tests
@@ -6,17 +8,66 @@ namespace Assignment5.Tests
     [TestClass]
     public class TestRealConsole
     {
-        private RealConsole consoleToUse;
+        private RealConsole _consoleToUse;
 
         [TestInitialize]
         public void Initialize()
         {
-            consoleToUse = new RealConsole();
+            _consoleToUse = new RealConsole();
         }
         
         [TestMethod]
-        public void IConsoleWriteLine_TestValidOutput()
+        [DataRow("test")]
+        [DataRow("C#")]
+        [DataRow("something with spaces")]
+        [DataRow("something with special characters! % &")]
+        public void IConsoleWriteLine_TestValidOutput(string toPrint)
         {
+            string capturedOutput = CaptureOutput(toPrint, _consoleToUse.WriteLine);
+            
+            Assert.AreEqual($"{toPrint}{System.Environment.NewLine}", capturedOutput);
+        }
+        
+        [DataTestMethod]
+        [DataRow("test")]
+        [DataRow("C#")]
+        [DataRow("something with spaces")]
+        [DataRow("something with special characters! % &")]
+        public void IConsoleWriteLine_TestValidInput(string toRead)
+        {
+            string capturedOutput = CaptureInput(toRead, _consoleToUse.ReadLine);
+            
+            Assert.AreEqual($"{toRead}", capturedOutput);
+        }
+
+        private static string CaptureOutput(string toTest, Action<string> methodName)
+        {
+            StringWriter stringWriter = new StringWriter();
+
+            var oldOut = Console.Out;
+            
+            Console.SetOut(stringWriter);
+
+            methodName(toTest);
+            
+            Console.SetOut(oldOut);
+
+            return stringWriter.ToString();
+        }
+        
+        private static string CaptureInput(string toTest, Func<string> methodName)
+        {
+            StringReader stringReader = new StringReader(toTest);
+
+            var oldIn = Console.In;
+            
+            Console.SetIn(stringReader);
+
+            string toReturn = methodName();
+            
+            Console.SetIn(oldIn);
+
+            return toReturn;
         }
     }
 }
--- Assignment5/Assignment5/Program.cs
+++ Assignment5/Assignment5/Program.cs
@@ -5,7 +5,7 @@ namespace Assignment5
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             // TODO: Allow user to select what event to create
             // TODO: Display list of created events
