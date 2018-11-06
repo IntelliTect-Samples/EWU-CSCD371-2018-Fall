70176bd51e6aa9ec7d08f26f8e2cdaca5497f278
Cameron Osborn <cameron0401@gmail.com>
2018-11-06 14:21:10 -0800

    Create new TestClass for testing struct characteristics.

---
+++ Assignment6/Assignment6.Tests/StructTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment6.Tests
+{
+    [TestClass]
+    public class StructTests
+    {
+        [TestMethod]
+        public static void TestMethod1()
+        {
+            
+        }
+    }
+}
