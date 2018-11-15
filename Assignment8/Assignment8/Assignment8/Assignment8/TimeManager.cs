6002909c52603482e2c0e4faa2c53406c00ba8eb
Casey White <caseywhite534@gmail.com>
2018-11-14 23:28:04 -0800

    Created TimeManager class

--- Assignment8/Assignment8/Assignment8/Assignment8/TimeManager.cs
+++ Assignment8/Assignment8/Assignment8/Assignment8/TimeManager.cs
@@ -1,14 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace Assignment8
 {
-    class TimeManager
+    public class TimeManager : INotifyPropertyChanged
     {
 
+        private readonly DispatcherTimer _timer;
+        private DateTime _LastTickTime;
+        public DateTime ElapsedTime { get; private set; }
+
+
+
+
+        public TimeManager()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(.1);
+            _timer.Tick += TimerOnClick;
+            _currentTime = "00:00:00";
+        }
+        
+        
+        
+        private string _currentTime;
+        public string CurrentTime
+        {
+            get { return _currentTime; }
+            set { 
+                    if (_currentTime != value)
+                    {
+                        _currentTime = value;
+                        OnCurrentTimeChanged();
+                    }
+                }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnCurrentTimeChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void StopButtonClick()
+        {
+            CurrentTime = "00:00:00";
+            _timer.Stop();
+        }
+
+        public void StartButtonClick()
+        {
+            _LastTickTime = DateTime.Now;
+            _timer.Start();
+        }
+
+        private void TimerOnClick(object sender, EventArgs e)
+        {
+            ElapsedTime += DateTime.Now - _LastTickTime;
+            _LastTickTime = DateTime.Now;
+            CurrentTime = ElapsedTime.ToString("mm:ss.FF");
+        }
+        
+        
 
     }
 }
