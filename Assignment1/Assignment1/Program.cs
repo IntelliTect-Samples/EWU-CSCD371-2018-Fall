d5f4ddb8108d2df6524503b76d7ecce2303c2155
Chester Southwood <csouthwood@eagles.ewu.edu>
2018-12-02 07:37:20 -0800

    AssignmentInProgress

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Assignment1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is Your Name?");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Hello Human: {0}!", userInput);
+            //Console.WriteLine($@"Hello {userInput}!");
+        }
+    }
+}
